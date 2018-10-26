022143329669addd8659962341ad7c73a21068a4
saahilk <kamatsaahil@gmail.com>
2018-10-26 16:16:25 +0530

    Add emergency activity

--- app/src/main/java/com/example/geforce/nitcalert/MainActivity.java
+++ app/src/main/java/com/example/geforce/nitcalert/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.geforce.nitcalert;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void add_emergency(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(MainActivity.this, AddEmergency.class);
+        startActivity(intent);
     }
 }
